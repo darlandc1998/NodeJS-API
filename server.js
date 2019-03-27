7058213e0372ebc79a30e3e5e239ef342742524e
darlan <darlandc1998>
2019-03-27 01:28:31 -0300

    Novos endpoints

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const expres = require('express');
+const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 //Iniciando o app
-const app = expres();
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 //Iniciando o DB
 mongoose.connect(
@@ -13,7 +16,5 @@ requireDir('./src/models');
 
 //Primeira rota
 app.use('/api', require('./src/routes'));
-
-
 
 app.listen(3001);
